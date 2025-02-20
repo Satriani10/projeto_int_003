1f47cd09da26aae1609296d3510e07f356c9136c
Brakister <smashbored@gmail.com>
2025-02-20 10:05:06 -0300

    painel administrativo agora suporta + de 1 livro

--- Library/biblioteca/livros/admin.py
+++ Library/biblioteca/livros/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Book, Tag, Borrow
 
-admin.site.register(Book)
+#admin.site.register(Book)
 admin.site.register(Tag)
 admin.site.register(Borrow)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'quantity', 'available_quantity', 'available')
+    list_filter = ('available',)
+    search_fields = ('title', 'author')
--- Library/biblioteca/livros/models.py
+++ Library/biblioteca/livros/models.py
@@ -6,8 +6,19 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+    
 
 class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    available = models.BooleanField(default=True)  # Indica se o livro está disponível no geral
+    quantity = models.PositiveIntegerField(default=1)  # Quantidade total de cópias
+    available_quantity = models.PositiveIntegerField(default=1)  # Cópias disponíveis atualmente
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+#class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, blank=True, null=True)
     available = models.BooleanField(default=True)
