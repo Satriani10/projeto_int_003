cfaa9c1875aaf7ad07cafdbaab62ee60f13f37ce
Brakister <smashbored@gmail.com>
2025-02-26 18:25:15 -0300

    inicializador, por padrão, abre no nav padrão

--- iniciar.bat
+++ iniciar.bat
@@ -1,8 +1,13 @@
+@echo off
 cd /d "%~dp0Library\biblioteca"
 
 :: Inicia o servidor Django
 echo Iniciando servidor Django...
-python manage.py runserver
+start /B python manage.py runserver
+
+timeout /t 5 /nobreak > nul
+
+start http://127.0.0.1:8000/
 
 ENDLOCAL
 pause
